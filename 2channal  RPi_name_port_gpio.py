a63d399e255bf7e17099e9e9c4ff370328a786f0
RaameshS <40315457+RaameshS@users.noreply.github.com>
2018-06-19 16:28:15 +0530

    Update 2channal  RPi_name_port_gpio.py

--- 2channal  RPi_name_port_gpio.py
+++ 2channal  RPi_name_port_gpio.py
@@ -13,7 +13,7 @@ class device_handler(debounce_handler):
        and the IP address of the Echo making the request.
     """
     #TRIGGERS = {str(sys.argv[1]): int(sys.argv[2])}
-    #TRIGGERS = {"office": 52000}
+    #TRIGGERS = {"office": 52000,"bed room":52000}
     TRIGGERS = {"kitchen": 52000,"living room":51000}
 
     def act(self, client_address, state, name):
