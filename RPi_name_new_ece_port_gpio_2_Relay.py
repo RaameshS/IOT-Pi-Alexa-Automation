fbaee59d335ccee2c4d02edea6a072661bd03f54
RaameshS <40315457+RaameshS@users.noreply.github.com>
2018-06-30 16:06:07 +0530

    RPi_name_new_ece_port_gpio_2_Relay.py

---
+++ RPi_name_new_ece_port_gpio_2_Relay.py
@@ -0,0 +1,63 @@
+import fauxmo
+import logging
+import time
+import sys
+import RPi.GPIO as GPIO ## Import GPIO library
+ 
+from debounce_handler import debounce_handler
+ 
+logging.basicConfig(level=logging.DEBUG)
+ 
+class device_handler(debounce_handler):
+    """Publishes the on/off state requested,
+       and the IP address of the Echo making the request.
+    """
+    #TRIGGERS = {str(sys.argv[1]): int(sys.argv[2])}
+    #TRIGGERS = {"office": 52000}
+    TRIGGERS = {"light": 53000,= "fan": 52004}
+
+    def act(self, client_address, state, name):
+        print("State", state, "from client @", client_address)
+        # GPIO.setmode(GPIO.BOARD) ## Use board pin numbering
+        # GPIO.setup(int(5), GPIO.OUT)   ## Setup GPIO Pin to OUTPUT
+        # GPIO.output(int(5), state) ## State is true/false
+        if name=="light":
+            GPIO.setmode(GPIO.BOARD) ## Use board pin numbering
+            GPIO.setup(int(5), GPIO.OUT)   ## Setup GPIO Pin to OUTPUT
+            GPIO.output(int(5), state) ## State is true/false
+        elif name == "fan":
+            GPIO.setmode(GPIO.BOARD)  ## Use board pin numbering
+            GPIO.setup(int(8), GPIO.OUT)  ## Setup GPIO Pin to OUTPUT
+            GPIO.output(int(8), state)  ## State is true/false
+        
+        else:
+            print("Device not found!")
+
+
+
+
+        return True
+ 
+if __name__ == "__main__":
+    # Startup the fauxmo server
+    fauxmo.DEBUG = True
+    p = fauxmo.poller()
+    u = fauxmo.upnp_broadcast_responder()
+    u.init_socket()
+    p.add(u)
+ 
+    # Register the device callback as a fauxmo handler
+    d = device_handler()
+    for trig, port in d.TRIGGERS.items():
+        fauxmo.fauxmo(trig, u, p, None, port, d)
+ 
+    # Loop and poll for incoming Echo requests
+    logging.debug("Entering fauxmo polling loop")
+    while True:
+        try:
+            # Allow time for a ctrl-c to stop the process
+            p.poll(100)
+            time.sleep(0.1)
+        except Exception as e:
+            logging.critical("Critical exception: "+ e.args  )
+            break
